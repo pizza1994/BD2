c4708aaadb67f62cd69530a5af246fbf4a0c07db
Luca Pitzalis <lucapitzalis@MacBook-Pro-di-Luca.local>
2017-06-23 12:54:30 +0200

    First commit

---
+++ ProgettoBD2/ExsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ProgettoBD2
+//
+//  Created by Luca Pitzalis on 23/06/17.
+//  Copyright © 2017 Luca Pitzalis. All rights reserved.
+//
+
+import UIKit
+
+class ExsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+     let exercises: [String] = ["Bench Press", "Inclined Bench Press", "Shoulder Press", "Squat", "Dumbell Curl", "French Press"]
+    
+    let cellReuseIdentifier = "cell"
+    @IBOutlet var tableView: UITableView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.exercises.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.exercises[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let SSController = self.storyboard?.instantiateViewController(withIdentifier: "SSController") as! StartStopViewController
+        
+        self.navigationController?.pushViewController(SSController, animated: true)
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ ProgettoBD2/StartStopViewController.swift
@@ -0,0 +1,52 @@
+//
+//  StartStopViewController.swift
+//  ProgettoBD2
+//
+//  Created by Luca Pitzalis on 23/06/17.
+//  Copyright © 2017 Luca Pitzalis. All rights reserved.
+//
+
+import UIKit
+
+class StartStopViewController: UIViewController {
+
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func startStopToggle(button: UIButton){
+        
+        if(button.tag == 0){
+            
+            button.tag = 1;
+        }
+        else{
+            button.tag = 0;
+        }
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
