db75ac299148f22db5ae96a71285f3a39ae3e5d1
Luca Pitzalis <lucapitzalis@MacBook-Pro-di-Luca.local>
2017-06-29 12:17:48 +0200

    Schermata benvenuto

--- ProgettoBD2/AppDelegate.swift
+++ ProgettoBD2/AppDelegate.swift
@@ -28,6 +28,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if(UserDefaults.standard.object(forKey: "firstTime") == nil){
+        let welcomeController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "wController") as! WelcomeViewController
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window?.rootViewController = welcomeController
+            UserDefaults.standard.set("notFirst", forKey: "firstTime")
+        }
         return true
     }
 
