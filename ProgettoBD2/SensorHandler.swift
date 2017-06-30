93df8f42e4f5c29a3850aa01b4c3bd8922a47deb
sirbardo <bardo_italiano@hotmail.it>
2017-06-30 12:04:45 +0200

    Temperature should be working

--- ProgettoBD2/SensorHandler.swift
+++ ProgettoBD2/SensorHandler.swift
@@ -29,42 +29,44 @@ class SensorHandler : NSObject, ESTTriggerManagerDelegate {
         ESTConfig.setupAppID("prova1-ft3", andAppToken: "efb8f4d5803312bdb2cf7c1bdd29ef55")
         
         
+        if (temperature == -255){
+            
+            let tempNotification = ESTTelemetryNotificationTemperature { (tempInfo) in
+                if (tempInfo.shortIdentifier == "eca7f68368e84571"){
+                    self.temperature = Double(tempInfo.temperatureInCelsius)
+                }
+            }
+
+            deviceManager.register(forTelemetryNotification: tempNotification)
+            
+        }
+
+        
         accNotification = ESTTelemetryNotificationMotion { (moveInfo) in
             if (moveInfo.shortIdentifier == "eca7f68368e84571"){
                 self.accData.append(Double(moveInfo.accelerationX))
             }
         }
         
+        
+        
         deviceManager.register(forTelemetryNotification: accNotification!)
+
+        
     }
     
     func stopListening(){
         deviceManager.unregister(forTelemetryNotification: accNotification!)
+        deviceManager.unregister(forTelemetryNotification: tempNotification)
     }
     
     func createExercise(exerciseName: String, weight : Double, nReps : Int) -> Exercise{
-        
-        let tempNotification = ESTTelemetryNotificationTemperature { (tempInfo) in
-            if (tempInfo.shortIdentifier == "eca7f68368e84571"){
-                self.temperature = Double(tempInfo.temperatureInCelsius)
-            }
-        }
-        
-        deviceManager.register(forTelemetryNotification: tempNotification)
-        stopListeningTemperature(tempNotification: tempNotification)
-        
-        
         
         exercise = Exercise(exerciseName: exerciseName, temperature: temperature, date: NSDate.init(timeIntervalSinceReferenceDate: Date.timeIntervalSinceReferenceDate), weight: weight, set: repData(nReps: nReps))
         
         accData.removeAll()
         
         return exercise!
-    }
-    
-    func stopListeningTemperature(tempNotification : ESTTelemetryNotificationTemperature){
-        //while (temperature == -255){}
-        deviceManager.unregister(forTelemetryNotification: tempNotification)
     }
     
     func addNewSet(weight : Double, nReps : Int){
