f2dae8d6dab50e9768c0e5708b1277d701e72a7b
sirbardo <bardo_italiano@hotmail.it>
2017-06-30 11:41:56 +0200

    bugfix

--- ProgettoBD2/SensorHandler.swift
+++ ProgettoBD2/SensorHandler.swift
@@ -63,7 +63,7 @@ class SensorHandler : NSObject, ESTTriggerManagerDelegate {
     }
     
     func stopListeningTemperature(tempNotification : ESTTelemetryNotificationTemperature){
-        while (temperature == -255){}
+        //while (temperature == -255){}
         deviceManager.unregister(forTelemetryNotification: tempNotification)
     }
     
@@ -94,7 +94,7 @@ class SensorHandler : NSObject, ESTTriggerManagerDelegate {
         diffAccArr.removeLast(diffAccArr.count - nReps)
         diffAccArr = diffAccArr.sorted(by: {$0.pos < $1.pos})
         
-        for i in 0...diffAccArr.count{
+        for i in 0...diffAccArr.count-1{
         
             if (accData[diffAccArr[i].pos] < accData[diffAccArr[i].pos+1]){
                 repData.append(accData[diffAccArr[i].pos+1])
