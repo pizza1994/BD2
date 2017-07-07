cea9a154e733c64dc8e4a1f4810c38f35098d9e5
Luca Pitzalis <lucapitzalis@macbook-pro-di-luca.local>
2017-07-07 12:05:53 +0200

    jfknbjkb df

--- ProgettoBD2/DB.swift
+++ ProgettoBD2/DB.swift
@@ -23,13 +23,15 @@ class DB: NSObject
         
     }
     
-    private static func perform(_ request: URLRequest) {
+    private static func perform(_ request: URLRequest) -> Data
+    {
         let _ = client.perform(request) {
             result in
             
             switch result {
             case let .success(response):
                 print("Success \(response)")
+                
                 
             case let .failure(error):
                 print("Error \(error)")
@@ -58,17 +60,22 @@ class DB: NSObject
     static func loadFromDb(name: String?, dateInterval : [String?], tempInterval: [String?], setInterval: [String?], repInterval: [String?], returnType : Int!)
     {
         connect()
-          
                         
         var params : Array<String> = Array<String>()
         
         if (name != "")
         {
             let names = name!.components(separatedBy: ", ")
-            for n in names{
-                params.append("\"exercise_name\": " + "\"" + n + "\"")
+            var or = "{$or: {"
+            for n in names
+            {
+                or = or + ("\"exercise_name\": " + "\"" + n + "\", ")
             }
             
+            or = or.substring(to: or.index(before: or.endIndex))
+            
+            or = or + "}"
+            params.append(or);
         }
         
         if (dateInterval[0]! != "") && (dateInterval[1]! != "")
@@ -83,23 +90,29 @@ class DB: NSObject
         
         if (tempInterval[0]! != "") && (tempInterval[1]! != "")
         {
-            params.append("\"temperature\": {$gt: " + String(tempInterval[0]!) + "}, \"date\":  {$lt: " + tempInterval[1]! + "}")
+            params.append("\"temperature\": {$gt: " + String(tempInterval[0]!) + "}, \"temperature\":  {$lt: " + tempInterval[1]! + "}")
             
         }
         
         if (setInterval[0]! != "") && (setInterval[1]! != "")
         {
-            params.append("\"set\": {$gt: " + setInterval[0]! + "}, \"date\":  {$lt: " + setInterval[1]! + "}")
+            params.append("\"n_sets\": {$gt: " + setInterval[0]! + "}, \"n_sets\":  {$lt: " + setInterval[1]! + "}")
             
         }
         
-        if (repInterval[0]! != "") && (setInterval[1]! != "")
-        {
-            params.append("\"set\": {$gt: " + setInterval[0]! + "}, \"date\":  {$lt: " + setInterval[1]! + "}")
-           
-        }
+        var value = params.joined(separator: ",")
         
-        let value = params.joined(separator: ",")
+        value = "{" + value + "}"
+        
+        switch returnType
+        {
+            case 0:
+                value = value + "{\"exercise_name\": 1, \"sets\": 1}" //prendere solo i set con abbastanza ripetizioni dell'insieme di set restituito
+            case 1:
+                value = value + "{\"exercise_name\": 1, \"sets\": 1, \"temperature\": 1}"
+            default:
+                value = value + "{\"exercise_name\": 1, \"calories\": 1}"
+        }
         
         let param = URLRequest.QueryStringParameter(key: "q", value: "{" + value + "}")
 
@@ -107,7 +120,36 @@ class DB: NSObject
             let request = try MongoLabURLRequest.urlRequestWith(configuration!, relativeURL: "collections/exercises", method: .GET, parameters: [param], bodyData: nil)
             
             print(request)
-            perform(request)
+            var response : Data = perform(request)
+            
+            let json = try? JSONSerialization.jsonObject(with: response, options: []) as! NSDictionary
+            
+
+            if (returnType == 0)
+            {
+                
+                /* [id: {"exercise_name": "nome1", "sets": rfjkdfb}, id: {"exercise_name": "nome1", "sets": rfjkdfb}]*/
+                var keyValues : NSDictionary
+                var average : Int = 0
+                var n : Int = 0
+                var setValues : Array<Double>
+                
+                for element in json!
+                {
+       
+                        if (element.key == "sets"){
+                            setValues.append(element.value as! Double)
+                        }
+                        
+                        tot = tot + rep
+                        n = n + 1
+                    
+                }
+                average = average / n
+                keyValues.
+                
+            }
+            
             
         } catch let error {
             print("Error \((error as? ErrorDescribable ?? MongoLabError.requestError).description())")
